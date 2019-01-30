73d0bd11929ed59cfe4fb8a370dbfa8acec433f0
Yixu Chen <cyx709671676@gmail.com>
2019-01-30 09:11:43 -0600

    Class and container

--- 1-Ruby new/1.5-Blocks and Iterators.rb
+++ 1-Ruby new/1.5-Blocks and Iterators.rb
@@ -1,4 +1,5 @@
-# {puts "hello"} # a single line code block
+# a single line code block
+# {puts "hello"}
 
 # a multiline code block
 # do
@@ -7,47 +8,51 @@
 # end
 
 #eg
-# def call_block
-#   puts "Start"
-#   yield
-#   yield
-#   puts "End"
-# end
-# call_block {puts "in the block"}
+def call_block
+  puts "Start"
+  yield
+  yield
+  puts "End"
+end
+call_block {puts "in the block"}
 
 #eg
 animals = %w( ant bee cat dog elk)
 animals.each{|animals| puts animals}
 animals.each{|name| print name, " "}
 5.times {print "*"}
+5.times {|a| print a}
 3.upto(6) {|i| print i}
 ('a'..'e').each {|char| print char}
+
 #eg
-# class Person
-#     def initialize( name )
-#          @name = name
-#     end
-#
-#     def do_with_name
-#         yield( @name )
-#     end
-# end
-#
-# person = Person.new("Oscar")
-#
-# #invoking the method passing a block
-# person.do_with_name do |name|
-#     puts "Hey, his name is #{name}"
-# end
-#
-# #invoke the method passing a different block
-# person.do_with_name do |name|
-#     puts "Hey, his name is #{name.reverse}"
-# end
-#
-#
-# days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
-# # select those which start with 't'
-# days.select do |item|
-#   item.match /^t/
-# end
+class Person
+    def initialize( name )
+         @name = name
+    end
+
+    def do_with_name
+      puts "Start"
+      yield( @name )
+      puts "End"
+    end
+end
+
+person = Person.new("Oscar")
+
+#invoking the method passing a block
+person.do_with_name do |name|
+    puts "Hey, his name is #{name}"
+end
+
+#invoke the method passing a different block
+person.do_with_name do |name|
+    puts "Hey, his name is #{name.reverse}"
+end
+
+
+days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+# select those which start with 't'
+days.select do |item|
+  item.match /^t/
+end
